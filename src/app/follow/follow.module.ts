ccdaf4c4fa2f00e035488622e193bb0b73948589
teymur00s64 <teymur00s64@gmail.com>
2024-09-09 15:20:30 +0300

    error fix

--- src/app/follow/follow.module.ts
+++ src/app/follow/follow.module.ts
@@ -1,12 +1,12 @@
 import { Module } from "@nestjs/common";
 import { TypeOrmModule } from "@nestjs/typeorm";
-import { FollowEntity } from "src/database/entities/Follow.entity";
+import { Follow } from "src/database/entities/Follow.entity";
 import { FollowController } from "./follow.controller";
 import { FollowService } from "./follow.service";
 import { UserModule } from "../user/user.module";
 
 @Module({
-    imports: [TypeOrmModule.forFeature([FollowEntity]), UserModule],
+    imports: [TypeOrmModule.forFeature([Follow]), UserModule],
     controllers: [FollowController],
     providers: [FollowService],
     exports: [FollowService],
